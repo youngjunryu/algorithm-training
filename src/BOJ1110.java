4dbc4f8518c976bc8971829df1d813fd4de2442d
ecvheo1 <ecvheo@naver.com>
2021-12-31 12:06:27 +0900

    Solve BOJ 1065 & BOJ 1110 & BOJ 4673

---
+++ src/BOJ1110.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class BOJ1110 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        br.close();
+
+        int newNumber = n;
+        int count = 0;
+
+        while(true) {
+            count++;
+            newNumber = (newNumber % 10 * 10) + ((newNumber / 10 + newNumber % 10) % 10);
+            if(newNumber == n) {
+                System.out.print(count);
+                break;
+            }
+        }
+    }
+}
