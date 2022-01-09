1b9d13bc5c83133f66d200e12740dcf0e97853bb
ecvheo1 <ecvheo@naver.com>
2022-01-09 14:29:29 +0900

    Solve BOJ 17298

---
+++ src/BOJ17298.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+public class BOJ17298 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Stack<Integer> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        String[] num = br.readLine().split(" ");
+        int[] result = new int[n];
+
+        for(int i = n - 1; i >= 0; i--) {
+            while(!stack.isEmpty() && Integer.parseInt(num[i]) >= stack.peek()) {
+                stack.pop();
+            }
+
+            if(stack.isEmpty())
+                result[i] = -1;
+            else
+                result[i] = stack.peek();
+
+            stack.push(Integer.parseInt(num[i]));
+        }
+
+        for(int i = 0; i < n; i++)
+            sb.append(result[i]).append(' ');
+        System.out.println(sb);
+    }
+}
