ae91a5d5a122b8ba92c2475acedb75d199c57c74
ecvheo1 <ecvheo@naver.com>
2022-02-14 14:20:29 +0900

    Solve PROGRAMMERS

---
+++ src/PROGRAMMERS/큰_수_만들기.java
@@ -0,0 +1,22 @@
+package PROGRAMMERS;
+
+public class 큰_수_만들기 {
+    public String solution(String number, int k) {
+        StringBuilder sb = new StringBuilder();
+        int pos = 0;
+
+        for(int i = 0; i < number.length() - k; i++) {
+            int max = 0;
+            for(int j = pos; j <= k + i; j++) {
+                if(max < number.charAt(j) - '0') {
+                    max = number.charAt(j) - '0';
+                    pos = j + 1;
+                }
+            }
+
+            sb.append(max);
+        }
+
+        return sb.toString();
+    }
+}
