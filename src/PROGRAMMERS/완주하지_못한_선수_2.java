8a285071995382df98bc72f5ae1b075dde1b528e
ecvheo1 <ecvheo@naver.com>
2022-03-14 19:26:15 +0900

    Solve PROGRAMMERS 완주하지 못한 선수

---
+++ src/PROGRAMMERS/완주하지_못한_선수_2.java
@@ -0,0 +1,29 @@
+package PROGRAMMERS;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class 완주하지_못한_선수_2 {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        Map<String, Integer> map = new HashMap<>();
+
+        for(String str : participant) {
+            map.put(str, map.getOrDefault(str, 0) + 1);
+        }
+
+        for(String str : completion) {
+            if(map.get(str) == 1) {
+                map.remove(str);
+            } else {
+                map.put(str, map.get(str) - 1);
+            }
+        }
+        
+        for(String key : map.keySet()) {
+            answer = key;
+        }
+
+        return answer;
+    }
+}
