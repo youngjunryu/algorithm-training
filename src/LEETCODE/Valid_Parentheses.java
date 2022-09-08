0fe638b4b3ecd2301741b104a1cfd532c596f97b
ecvheo1 <ecvheo@naver.com>
2022-09-08 09:16:48 +0900

    Solve LEETCODE Valid Parentheses

---
+++ src/LEETCODE/Valid_Parentheses.java
@@ -0,0 +1,38 @@
+package LEETCODE;
+
+import java.util.Map;
+import java.util.Stack;
+
+public class Valid_Parentheses {
+  public boolean isValid(String s) {
+    Map<Character, Character> map = Map.of('(', ')', '{', '}', '[', ']');
+    Stack<Character> stack = new Stack<>();
+    boolean answer = true;
+
+    for (int i = 0; i < s.length(); i++) {
+      char cur = s.charAt(i);
+
+      if (cur == '(' || cur == '{' || cur == '[') {
+        stack.push(cur);
+      } else {
+        if (stack.isEmpty()) {
+          answer = false;
+          break;
+        } else {
+          if (map.get(stack.peek()) == cur) {
+            stack.pop();
+          } else {
+            answer = false;
+            break;
+          }
+        }
+      }
+    }
+
+    if (!stack.isEmpty()) {
+      answer = false;
+    }
+
+    return answer;
+  }
+}
