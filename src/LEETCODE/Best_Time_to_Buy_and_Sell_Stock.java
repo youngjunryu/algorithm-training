39820ffff2e7045dde8d7c83c26ba8b219184808
ecvheo1 <ecvheo@naver.com>
2022-09-09 16:17:03 +0900

    Solve LEETCODE Best Time to Buy and Sell Stock

---
+++ src/LEETCODE/Best_Time_to_Buy_and_Sell_Stock.java
@@ -0,0 +1,23 @@
+package LEETCODE;
+
+public class Best_Time_to_Buy_and_Sell_Stock {
+  public int maxProfit(int[] prices) {
+    int min = 10001;
+    int max = 0;
+
+    for (int i = 0; i < prices.length; i++) {
+      min = Math.min(min, prices[i]);
+      max = Math.max(max, prices[i] - min);
+    }
+
+    return max;
+  }
+}
+
+/*
+
+7 1 5 3 6 4
+0 1 2 3 4 5
+
+
+ */
