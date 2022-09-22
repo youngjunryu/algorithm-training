a4a611adde7ac903a8537bf70629f87541050ee1
ecvheo1 <ecvheo@naver.com>
2022-09-22 11:13:02 +0900

    Solve LEETCODE Contains Duplicate

---
+++ src/LEETCODE/Contains_Duplicate.java
@@ -0,0 +1,19 @@
+package LEETCODE;
+
+import java.util.HashMap;
+
+public class Contains_Duplicate {
+  public boolean containsDuplicate(int[] nums) {
+    HashMap<Integer, Integer> map = new HashMap<>();
+
+    for (int n : nums) {
+      if (map.containsKey(n)) {
+        return true;
+      }
+
+      map.put(n, 1);
+    }
+
+    return false;
+  }
+}
