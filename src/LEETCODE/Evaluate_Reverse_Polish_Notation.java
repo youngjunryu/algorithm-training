8d8b0cbc28a645b8e58e45e664497f380f902ae2
ecvheo1 <ecvheo@naver.com>
2022-09-30 16:51:25 +0900

    Solve LEETCODE Evaluate Reverse Polish Notation

---
+++ src/LEETCODE/Evaluate_Reverse_Polish_Notation.java
@@ -0,0 +1,46 @@
+package LEETCODE;
+
+import java.util.Stack;
+
+public class Evaluate_Reverse_Polish_Notation {
+  static Stack<Integer> s;
+
+  public static int evalRPN(String[] tokens) {
+    s = new Stack<>();
+
+    for (String t : tokens) {
+      int[] nums;
+      switch (t) {
+        case "*":
+          nums = getNums();
+          s.push(nums[0] * nums[1]);
+          break;
+        case "/":
+          nums = getNums();
+          s.push(nums[1] / nums[0]);
+          break;
+        case "+":
+          nums = getNums();
+          s.push(nums[0] + nums[1]);
+          break;
+        case "-":
+          nums = getNums();
+          s.push(nums[1] - nums[0]);
+          break;
+        default:
+          s.push(Integer.parseInt(t));
+          break;
+      }
+    }
+
+    return s.peek();
+  }
+
+  private static int[] getNums() {
+    return new int[]{s.pop(), s.pop()};
+  }
+
+  public static void main(String[] args) {
+    evalRPN(new String[]{"10","6","9","3","+","-11","*","/","*","17","+","5","+"});
+  }
+}
