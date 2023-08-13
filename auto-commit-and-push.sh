1c263d53edd3a09e4cfe1c51ca596599a1028f4f
ecvheo1 <ecvheo@naver.com>
2023-08-14 00:17:05 +0900

    :sparkles: Git 커밋 메시지 자동 완성 스크립트 구현

---
+++ auto-commit-and-push.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+CHANGED_FILES=$(git diff --name-only)
+
+git add .
+
+PROBLEM_NAME=$(echo "$CHANGED_FILES" | awk -F'/' '{ for (i=2; i<NF; i++) print $i }' | tr '\n' '.' | sed 's/\.$//')
+
+COMMIT_MESSAGE="Solve: $PROBLEM_NAME"
+
+git commit -m "$COMMIT_MESSAGE"
+
+git push origin main
