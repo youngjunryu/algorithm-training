e446715d6f88750c0b6d253b73703813fd95db64
ecvheo1 <ecvheo@naver.com>
2023-08-14 10:28:53 +0900

    Solve: LEETCODE.easy.Find_the_Index_of_the_First_Occurrence_in_a_String

---
+++ src/LEETCODE/easy/Find_the_Index_of_the_First_Occurrence_in_a_String/Solution.java
@@ -0,0 +1,8 @@
+package LeetCode.easy.Find_the_Index_of_the_First_Occurrence_in_a_String;
+
+class Solution {
+  public int strStr(String haystack, String needle) {
+    int result = haystack.indexOf(needle);
+    return result;
+  }
+}
