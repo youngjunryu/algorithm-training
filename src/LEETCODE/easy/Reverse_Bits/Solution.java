bb574786995ac4d1936d328818d8a05bb2d76589
ecvheo1 <ecvheo@naver.com>
2023-08-25 16:13:45 +0900

    Solve: LEETCODE.easy.Reverse_Bits

---
+++ src/LEETCODE/easy/Reverse_Bits/Solution.java
@@ -0,0 +1,16 @@
+package LeetCode.easy.Reverse_Bits;
+
+public class Solution {
+  public int reverseBits(int n) {
+    int result = 0;
+
+    for (int i = 0; i < 32; i++) {
+      result <<= 1;
+      result |= (n & 1);
+      n >>= 1;
+    }
+
+    return result;
+  }
+}
+
