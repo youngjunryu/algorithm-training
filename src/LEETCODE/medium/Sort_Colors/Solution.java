3ae9f06aef7324a707c8e0965c9087157b24b161
ecvheo1 <ecvheo@naver.com>
2023-09-16 11:59:06 +0900

    Solve: LEETCODE.medium.Sort_Colors

---
+++ src/LEETCODE/medium/Sort_Colors/Solution.java
@@ -0,0 +1,20 @@
+package LeetCode.medium.Sort_Colors;
+
+class Solution {
+  public void sortColors(int[] nums) {
+    HashMap<Integer, Integer> map = new HashMap<>();
+    for (int n : nums) {
+      map.put(n, map.getOrDefault(n, 0) + 1);
+    }
+
+    int idx = 0;
+    for (int i = 0; i < 3; i++) {
+      if (map.containsKey(i)) {
+        int cnt = map.get(i);
+        while (cnt-- > 0) {
+          nums[idx++] = i;
+        }
+      }
+    }
+  }
+}
