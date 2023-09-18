9d49445c5e2e77026bc195fa66a400d63aefc528
ecvheo1 <ecvheo@naver.com>
2023-09-18 18:58:05 +0900

    Solve: LEETCODE.medium.Jump_Game

---
+++ src/LEETCODE/medium/Jump_Game/Solution.java
@@ -0,0 +1,14 @@
+package LeetCode.medium.Jump_Game;
+
+class Solution {
+  public boolean canJump(int[] nums) {
+    int reachable = 0;
+
+    for (int i = 0; i < nums.length; i++) {
+      if (i > reachable) return false;
+      reachable = Math.max(reachable, i + nums[i]);
+    }
+
+    return true;
+  }
+}
