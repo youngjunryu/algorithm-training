171b97a727fd3983727eef3421abb20bca3e3194
ecvheo1 <ecvheo@naver.com>
2024-03-13 23:33:11 +0900

    Solve: Programmers.lv1.\353\221\220_\352\260\234_\353\275\221\354\225\204\354\204\234_\353\215\224\355\225\230\352\270\260

---
+++ src/Programmers/lv1/두_개_뽑아서_더하기/Solution.java
@@ -0,0 +1,18 @@
+package Programmers.lv1.두_개_뽑아서_더하기;
+
+import java.util.*;
+
+class Solution {
+  public int[] solution(int[] numbers) {
+    int[] answer = {};
+
+    HashSet<Integer> set = new HashSet<>();
+    for (int i = 0; i < numbers.length - 1; i++) {
+      for (int j = i + 1; j < numbers.length; j++) {
+        set.add(numbers[i] + numbers[j]);
+      }
+    }
+
+    return set.stream().sorted().mapToInt(Integer::intValue).toArray();
+  }
+}
