dfa5ab6f12b484dab37f4756a13e0436dc3f4616
ecvheo1 <ecvheo@naver.com>
2024-04-21 12:48:56 +0900

    Solve: LEETCODE.medium.Gas_Station

---
+++ src/LEETCODE/medium/Gas_Station/Solution.java
@@ -0,0 +1,47 @@
+package LeetCode.medium.Gas_Station;
+
+class Solution {
+
+  public int canCompleteCircuit(int[] gas, int[] cost) {
+    int n = gas.length;
+    int gasSum = 0;
+    int costSum = 0;
+
+    for (int i = 0; i < n; i++) {
+      gasSum += gas[i];
+      costSum += cost[i];
+    }
+
+    if (gasSum < costSum) {
+      return -1;
+    }
+
+    boolean flag = true;
+    int i = 0;
+    for (; i < n && flag; ) {
+      int val = gas[i];
+
+      int idx = i;
+      int next = -1;
+
+      while (true) {
+        if (val - cost[idx] < 0) {
+          i = (idx + 1) % n;
+          break;
+        }
+
+        next = (idx + 1) % n;
+
+        if (next == i) {
+          flag = false;
+          break;
+        }
+
+        val = val - cost[idx] + gas[next];
+        idx = next;
+      }
+    }
+
+    return i;
+  }
+}
