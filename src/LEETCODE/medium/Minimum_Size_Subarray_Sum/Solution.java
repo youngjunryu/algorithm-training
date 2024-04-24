62098f62caede6f9b93ec082edeb156a7f957c36
ecvheo1 <ecvheo@naver.com>
2024-04-25 03:30:15 +0900

    Solve: LEETCODE.medium.Minimum_Size_Subarray_Sum

---
+++ src/LEETCODE/medium/Minimum_Size_Subarray_Sum/Solution.java
@@ -0,0 +1,48 @@
+package LeetCode.medium.Minimum_Size_Subarray_Sum;
+
+public class Solution {
+
+  public int minSubArrayLen(int target, int[] nums) {
+    int left = 1;
+    int right = nums.length;
+    int mn = 0;
+
+    while (left <= right) {
+      int mid = (left + right) / 2;
+
+      if (windowCheck(mid, nums, target)) {
+        mn = left;
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
+    }
+
+    return mn;
+  }
+
+  private boolean windowCheck(int size, int[] nums, int target) {
+    int i = 0;
+    int j = 0;
+    int sum = 0;
+    int mn = Integer.MIN_VALUE;
+
+    while (j < nums.length) {
+      sum += nums[j];
+
+      while (j - i + 1 == size) {
+        mn = Math.max(sum, mn);
+        sum -= nums[i];
+        i++;
+      }
+
+      j++;
+    }
+
+    if (mn >= target) {
+      return true;
+    }
+    return false;
+  }
+}
+
