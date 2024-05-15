ee6454cbaa9d19012e507808ccb686398b36b357
ecvheo1 <ecvheo@naver.com>
2024-05-15 20:37:45 +0900

    Solve: Baekjoon._17266

--- src/Baekjoon/_17266/Main.java
+++ src/Baekjoon/_17266/Main.java
@@ -7,19 +7,57 @@ import java.util.StringTokenizer;
 
 public class Main {
 
+  static int[] arr;
+  static int N;
+  static int M;
+
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     StringTokenizer st;
 
-    int N = Integer.parseInt(br.readLine());
-    int M = Integer.parseInt(br.readLine());
-    int[] arr = new int[M];
+    N = Integer.parseInt(br.readLine());
+    M = Integer.parseInt(br.readLine());
+    arr = new int[M];
 
     st = new StringTokenizer(br.readLine());
     for (int i = 0; i < M; i++) {
       arr[i] = Integer.parseInt(st.nextToken());
     }
 
-    
+    int left = 1;
+    int right = 100000;
+
+    while (left <= right) {
+      int mid = (left + right) / 2;
+
+      boolean result = check(mid);
+      if (result) {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
+    }
+
+    System.out.println(left);
+  }
+
+  private static boolean check(int len) {
+    int prev = 0;
+
+    for (int i = 0; i < M; i++) {
+      int now = arr[i];
+
+      if (now - len <= prev) {
+        prev = now + len;
+      } else {
+        return false;
+      }
+    }
+
+    if (prev < N) {
+      return false;
+    }
+
+    return true;
   }
 }
