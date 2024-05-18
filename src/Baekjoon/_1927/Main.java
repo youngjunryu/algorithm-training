99a851133e2089f90b287e2c7ef3945d319eef4b
ecvheo1 <ecvheo@naver.com>
2024-05-18 11:28:55 +0900

    Solve: Baekjoon._1927

---
+++ src/Baekjoon/_1927/Main.java
@@ -0,0 +1,30 @@
+package Baekjoon._1927;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int N = Integer.parseInt(br.readLine());
+
+    PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+    for (int i = 0; i < N; i++) {
+      int num = Integer.parseInt(br.readLine());
+      if (num == 0) {
+        if (pq.isEmpty()) {
+          System.out.println("0");
+        } else {
+          System.out.println(pq.poll());
+        }
+      } else {
+        pq.add(num);
+      }
+    }
+  }
+}
