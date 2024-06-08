6c1bb9a0e9b26d0a7cbbc6e93a91845000fceec9
ecvheo1 <ecvheo@naver.com>
2024-06-08 11:19:27 +0900

    Solve: Programmers.\355\205\214\354\212\244\355\212\270

---
+++ src/Programmers/테스트/Solution.java
@@ -0,0 +1,5 @@
+package Programmers.테스트;
+
+public class Solution {
+
+}
---
+++ src/test.sh
@@ -0,0 +1,19 @@
+#!/bin/zsh
+
+# UTF-8 인코딩 설정
+export LANG=ko_KR.UTF-8
+export LC_ALL=ko_KR.UTF-8
+
+CHANGED_FILES=$(git diff --name-only)
+
+git add .
+
+# 문제 이름 추출 (폴더 구조에 따라 조정 필요)
+PROBLEM_NAME=$(echo "$CHANGED_FILES" | awk -F'/' '{ for (i=2; i<NF; i++) print $i }' | tr '\n' '.' | sed 's/\.$//')
+
+# 커밋 메시지 생성
+COMMIT_MESSAGE="Solve: $PROBLEM_NAME"
+
+# 커밋 및 푸시
+git commit -m "$COMMIT_MESSAGE"
+git push -u origin main
