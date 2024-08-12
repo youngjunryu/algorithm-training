f0a05c1d76a1224c791e2ef84ce6de856fa6898e
ecvheo1 <ecvheo@naver.com>
2024-08-12 19:35:27 +0900

    Solve: Baekjoon._10171

---
+++ src/Baekjoon/_10171/Main.java
@@ -0,0 +1,13 @@
+package Baekjoon._10171;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    System.out.println("\\    /\\");
+    System.out.println(" )  ( ')");
+    System.out.println("(  /  )");
+    System.out.println(" \\(__)|");
+
+  }
+}
