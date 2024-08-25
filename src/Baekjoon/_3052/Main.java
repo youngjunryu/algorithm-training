409a9b870cf35a72e10672fece24b387687d887c
ecvheo1 <ecvheo@naver.com>
2024-08-25 10:56:39 +0900

    Solve: Baekjoon._3052

---
+++ src/Baekjoon/_3052/Main.java
@@ -0,0 +1,21 @@
+package Baekjoon._3052;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    HashSet<Integer> set = new HashSet<>();
+    for (int i = 0; i < 10; i++) {
+      int num = Integer.parseInt(br.readLine());
+
+      set.add(num % 42);
+    }
+
+    System.out.println(set.size());
+  }
+}
