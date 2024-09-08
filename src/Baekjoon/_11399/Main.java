37bf09d86efdc6837faefd734a77894c831444e4
ecvheo1 <ecvheo@naver.com>
2024-09-08 23:19:27 +0900

    Solve: Baekjoon._11399

---
+++ src/Baekjoon/_11399/Main.java
@@ -0,0 +1,31 @@
+package Baekjoon._11399;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int N = Integer.parseInt(br.readLine());
+
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int[] arr = new int[N];
+    for (int i = 0; i < N; i++) {
+      arr[i] = Integer.parseInt(st.nextToken());
+    }
+
+    Arrays.sort(arr);
+
+    int result = 0;
+    for (int i = 0; i < N; i++) {
+      result += arr[i] * (N - i);
+    }
+
+    System.out.println(result);
+  }
+}
