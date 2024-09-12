126a7015902ebea3ca6b126be868f9e67b6411d0
ecvheo1 <ecvheo@naver.com>
2024-09-12 23:31:08 +0900

    Solve: Baekjoon._2606

---
+++ src/Baekjoon/_2606/Main.java
@@ -0,0 +1,57 @@
+package Baekjoon._2606;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+  static int N;
+  static ArrayList<ArrayList<Integer>> map;
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
+
+    N = Integer.parseInt(br.readLine());
+    map = new ArrayList<>();
+    for (int i = 0; i <= N; i++) {
+      map.add(new ArrayList<>());
+    }
+
+    int t = Integer.parseInt(br.readLine());
+    for (int i = 0; i < t; i++) {
+      st = new StringTokenizer(br.readLine());
+      int a = Integer.parseInt(st.nextToken());
+      int b = Integer.parseInt(st.nextToken());
+      map.get(a).add(b);
+      map.get(b).add(a);
+    }
+
+    Queue<Integer> q = new LinkedList<>();
+    q.add(1);
+    boolean[] visited = new boolean[N + 1];
+    visited[1] = true;
+    int result = 0;
+
+    while (!q.isEmpty()) {
+      int now = q.poll();
+
+      for (int next : map.get((now))) {
+        if (visited[next]) {
+          continue;
+        }
+
+        result++;
+        visited[next] = true;
+        q.add(next);
+      }
+    }
+
+    System.out.println(result);
+  }
+}
