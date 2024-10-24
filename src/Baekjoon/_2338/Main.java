30660b11725e29b2804e06bc34957cfac35de36d
ecvheo1 <ecvheo@naver.com>
2024-10-25 06:10:41 +0900

    Solve:

---
+++ src/Baekjoon/_2338/Main.java
@@ -0,0 +1,18 @@
+package Baekjoon._2338;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    BigInteger first = in.nextBigInteger();
+    BigInteger second = in.nextBigInteger();
+
+    System.out.println(first.add(second));
+    System.out.println(first.subtract(second));
+    System.out.print(first.multiply(second));
+
+  }
+}
